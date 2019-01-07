ee53592cb7abc14161cb3eb1721a1a099a51ab94
Stanisław Lutkiewicz <s17535@pjwstk.edu.pl>
2019-01-07 10:15:00 +0100

    initial

---
+++ JAZExtra/src/main/java/domain/Person.java
@@ -0,0 +1,88 @@
+package domain;
+
+import java.io.Serializable;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.validation.constraints.NotNull;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+@Entity
+@NamedQueries({
+	
+@NamedQuery(name="person.all", query="SELECT p FROM Person p"),
+@NamedQuery(name="person.page", query="SELECT p FROM Person p "
+		+ "where p.id >= :personPageFrom "
+		+ "and p.id <= :personPageTo ")
+
+})
+
+public class Person implements Serializable {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String firstName;
+	@NotNull
+	private String lastName;
+	@NotNull
+	private String eMail;
+	private String gender;
+	private int age;
+	private String birthday;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String geteMail() {
+		return eMail;
+	}
+	public void seteMail(String eMail) {
+		this.eMail = eMail;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getBirthday() {
+		return birthday;
+	}
+	public void setBirthday(String birthday) {
+		this.birthday = birthday;
+	}
+	
+
+}
---
+++ JAZExtra/src/main/java/rest/services/PersonResource.java
@@ -0,0 +1,92 @@
+package rest.services;
+
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import domain.Person;
+
+@Path("people")
+@Stateless
+public class PersonResource {
+	@PersistenceContext
+	EntityManager em;
+	/*//--------------GET ALL PERSON--------------
+		@GET
+		@Produces(MediaType.APPLICATION_JSON)
+		public List<Person> getAll(){
+			List<Person> result =em.createNamedQuery("person.all", Person.class).getResultList();
+			return result;
+		}*/
+		//--------------GET PERSON PAGE--------------
+		@GET
+		
+		@Produces(MediaType.APPLICATION_JSON)
+		public List<Person> getPage(@QueryParam("page") int page){
+			List<Person> result =em.createNamedQuery("person.page", Person.class)
+					.setParameter("personPageFrom", countPageQuantity(--page))
+					.setParameter("personPageTo", countPageQuantity(++page))
+					.getResultList();
+			return result;
+		}
+	// --------------ADD PERSON--------------
+		@POST
+		@Consumes(MediaType.APPLICATION_JSON)
+		public Response Add(Person person) {
+			//CHECK IF EXIST !!!!!!!!!!!!!!!!!!!!
+				
+			em.persist(person);
+			return Response.ok(person.getId()).build();
+		}
+	// --------------DELETE PERSON--------------
+		@DELETE
+		@Path("/{id}")
+		public Response DeleteP(@PathParam("id") int id) {
+			Person result = em.find(Person.class, id);
+			if(result==null) 
+				return Response.status(404).build();
+			em.remove(result);
+			return Response.ok().build();
+		}
+	//------------------EDIT PRODUCT-------------------
+		@PUT
+		@Path("/{id}")
+		@Consumes(MediaType.APPLICATION_JSON)
+		public Response update(@PathParam("id") int id,Person person) {
+			Person result = em.find(Person.class, id);
+			if(result==null )
+				return Response.status(404).build();
+			
+			em.persist(updatePerson(result,person));
+			return Response.ok().build();		
+		}
+		//////////////////////////////FUNKCJE POMOCNICZE
+
+		private Person updatePerson(Person origin,Person update) {
+			origin.setFirstName(update.getFirstName());
+			origin.setLastName(update.getLastName());
+			origin.seteMail(update.geteMail());
+			origin.setGender(update.getGender());
+			origin.setAge(update.getAge());
+			origin.setBirthday(update.getBirthday());
+			return origin;
+		}	
+		private int countPageQuantity(int page) {
+			int quantityPerPage=5;
+			return page*quantityPerPage;
+		}
+		
+		
+}
